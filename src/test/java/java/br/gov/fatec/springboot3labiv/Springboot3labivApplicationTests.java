bdf8a410180d2171b46afbbf88a0239092762aaf
Valdiney <valdiney.nascimento@fatec.sp.gov.br>
2023-08-16 15:26:32 -0300

    Carregando arquivo do spring initializr

---
+++ src/test/java/java/br/gov/fatec/springboot3labiv/Springboot3labivApplicationTests.java
@@ -0,0 +1,13 @@
+package java.br.gov.fatec.springboot3labiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3labivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
