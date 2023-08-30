f550b5eac58e8112e523f8f46bd95e0a50bfcd06
Valdiney <valdiney.nascimento@fatec.sp.gov.br>
2023-08-30 19:57:40 -0300

    Alteração da classe autorização.

--- src/main/java/br/gov/fatec/springbboot3labiv/entity/Autorizacao.java
+++ src/main/java/br/gov/fatec/springbboot3labiv/entity/Autorizacao.java
@@ -1,16 +1,16 @@
 package br.gov.fatec.springbboot3labiv.entity;
 
-import java.time.LocalDateTime;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,13 +25,13 @@ public class Autorizacao {
     @Column(name = "aut_nome")
     private String nome;
 
-     @Column(name = "ant_data_hora")
-    private LocalDateTime dataHora;
-
-    @ManyToOne
     @JsonIgnore
-    @JoinColumn(name = "ant_usr_id")
-    private Usuario usuario;
+    @ManyToMany(mappedBy = "autorizacoes", fetch = FetchType.LAZY)
+    private Set<Usuario> usuarios;
+
+    public Autorizacao(String nome) {
+        this.nome = nome;
+    }
 
     public String getNome() {
         return nome;
@@ -41,20 +41,12 @@ public class Autorizacao {
         this.nome = nome;
     }
 
-    public LocalDateTime getDataHora() {
-        return dataHora;
+    public Set<Usuario> getUsuarios() {
+        return usuarios;
     }
 
-    public void setDataHora(LocalDateTime dataHora) {
-        this.dataHora = dataHora;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
+    public void setUsuarios(Set<Usuario> usuarios) {
+        this.usuarios = usuarios;
     }
 
     
